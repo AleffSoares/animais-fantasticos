1a2a79189aa3ea26b4c15d0f086afb169c27f78e
Aleff Soares <aleffsoares8@gmail.com>
2022-08-06 14:18:58 -0400

    Accordion refatorado

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,17 +1,23 @@
-export default function initAccordion() {
-  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-  const ativoClasse = 'ativo';
-  if(accordionList.length) {
-    accordionList[0].classList.add(ativoClasse);
-    accordionList[0].nextElementSibling.classList.add(ativoClasse);
-    function activeAccordion() {
-      this.classList.toggle(ativoClasse);
-      this.nextElementSibling.classList.toggle(ativoClasse);
-    }
-
-    accordionList.forEach((item) =>{
-        item.addEventListener('click', activeAccordion);
-    })
+export default class Accordion {
+  constructor(list) {
+    this.accordionList = document.querySelectorAll(list);
+    this.activeClass = 'ativo';
   }
-  
+  toggleAccordion(item) {
+    item.classList.toggle(this.activeClass);
+    item.nextElementSibling.classList.toggle(this.activeClass);
+  }
+
+  addAccordionEvent() {
+      this.accordionList.forEach((item) =>{
+        item.addEventListener('click', () => this.toggleAccordion(item));
+      });
+  }
+
+  init() {
+    if (this.accordionList.length) {
+      this.toggleAccordion(this.accordionList[0]);
+      this.addAccordionEvent();
+    }
+  }
 }
